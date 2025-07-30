0a206a954e69e67588bac395e5f6c4e73bd5910a
Iftiaz-Ahmed <iftiaz.ahmed@yahoo.com>
2025-07-29 22:44:35 -0400

    custom text

---
+++ testing_script.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+def main():
+    print("This is a testing script, running at:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+if __name__ == "__main__":
+    main()
